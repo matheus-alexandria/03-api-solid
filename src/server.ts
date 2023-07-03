640162ab242c8c85dc3074cfc07e239870df6f8a
Matchobas <maalexandria@hotmail.com>
2023-07-02 22:36:55 -0300

    Add: config eslint

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import { app } from './app';
 import { env } from './env';
 
 app.listen({
-  host: "0.0.0.0",
-  port: env.PORT 
+  host: '0.0.0.0',
+  port: env.PORT,
 }, () => {
-  console.log("HTTP Server Online!");
+  console.log('HTTP Server Online!');
 });
