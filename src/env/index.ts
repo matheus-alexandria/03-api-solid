d7a38dca7332983bfb76075f422bb6a5ce599171
Matchobas <maalexandria@hotmail.com>
2023-07-03 22:00:20 -0300

    Fix: Lint fixes and lint command added

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,17 +1,17 @@
-import "dotenv/config";
+import 'dotenv/config';
 import { z } from 'zod';
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
 });
 
 const envParsed = envSchema.safeParse(process.env);
 
 if (envParsed.success === false) {
-  console.log("Invalid enviroment variables", envParsed.error.format());
+  console.log('Invalid enviroment variables', envParsed.error.format());
 
-  throw new Error("Invalid enviroment variables.");
+  throw new Error('Invalid enviroment variables.');
 }
 
 export const env = envParsed.data;
