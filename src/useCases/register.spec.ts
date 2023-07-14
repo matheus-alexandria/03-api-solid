97830cd7132dc620c0c1ea7cdd9adcaf626da21e
Matchobas <maalexandria@hotmail.com>
2023-07-14 14:47:48 -0300

    Reafactor: unit test with in memory database

--- src/useCases/register.spec.ts
+++ src/useCases/register.spec.ts
@@ -1,12 +1,24 @@
 import { describe, it, expect } from 'vitest';
-import { PrismaUserRepository } from '@/repositories/prismaUsersRepository';
 import { compare } from 'bcryptjs';
 import { RegisterUseCase } from './register';
 
 describe('Register Use Case', () => {
   it('should hash user password upon registration', async () => {
-    const prismaUsersRepository = new PrismaUserRepository();
-    const register = new RegisterUseCase(prismaUsersRepository);
+    const register = new RegisterUseCase({
+      async findByEmail(email) {
+        return null;
+      },
+
+      async create(data) {
+        return {
+          id: 'user-01',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      },
+    });
 
     const { user } = await register.execute({
       name: 'John Doe',
