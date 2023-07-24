0bef72b40fc0e5684b454f1dc465bd6aa9b35307
Matchobas <maalexandria@hotmail.com>
2023-07-24 11:32:35 -0300

    Test: second validate use case test added

--- src/useCases/validateCheckIn.spec.ts
+++ src/useCases/validateCheckIn.spec.ts
@@ -3,6 +3,7 @@ import {
 } from 'vitest';
 import { InMemoryCheckInsRepository } from '@/repositories/inMemory/inMemoryCheckInsRepository';
 import { ValidateCheckInUseCase } from './validateCheckIn';
+import { ResourceNotFoundError } from './errors/resourceNotFoundError';
 
 let inMemoryCheckInsRepository: InMemoryCheckInsRepository;
 let sut: ValidateCheckInUseCase;
@@ -33,5 +34,11 @@ describe('Validate Check In Use Case', () => {
 
     expect(checkIn.validated_at).toEqual(expect.any(Date));
     expect(inMemoryCheckInsRepository.items[0].validated_at).toEqual(expect.any(Date));
+  });
+
+  it.only('should not be able to validate an inexistent check in', async () => {
+    await expect(() => sut.execute({
+      checkInId: 'inexistent-check-in-id',
+    })).rejects.toBeInstanceOf(ResourceNotFoundError);
   });
 });
