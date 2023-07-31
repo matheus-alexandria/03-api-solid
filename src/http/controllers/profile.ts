426c4e99d78d0ca96548c060a4f70cdbbe692acf
Matchobas <maalexandria@hotmail.com>
2023-07-31 15:09:18 -0300

    Chore: polishing the create profile route

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,18 @@
+import { makeGetUserProfileUseCase } from '@/useCases/factories/makeGetUserProfileUseCase';
 import { FastifyReply, FastifyRequest } from 'fastify';
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify();
 
-  console.log(request.user.sub);
-  return reply.status(200).send();
+  const getProfileUseCase = makeGetUserProfileUseCase();
+  const { user } = await getProfileUseCase.execute({
+    userId: request.user.sub,
+  });
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 }
