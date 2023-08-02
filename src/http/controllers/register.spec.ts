d1acb2b86c38b490890d103b6211bf12e2601b0d
Matchobas <maalexandria@hotmail.com>
2023-08-01 21:40:26 -0300

    Test: first test with temp schema

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,26 @@
-import { test } from 'vitest';
+import request from 'supertest';
+import { app } from '@/app';
+import {
+  afterAll, beforeAll, describe, expect, it,
+} from 'vitest';
 
-test('ok', () => {
-  // empty
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+  it('should be able to register', async () => {
+    const response = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      });
+
+    expect(response.statusCode).toBe(201);
+  });
 });
