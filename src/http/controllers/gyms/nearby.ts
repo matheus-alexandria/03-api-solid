b14c7b0911bab37e6affc0a14e03c09578cd471a
Matchobas <maalexandria@hotmail.com>
2023-08-03 21:30:28 -0300

    Chore: other gym controllers and routes

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { makeFetchNeabyGymsUseCase } from '@/useCases/factories/makeFetchNearbyGymsUseCase';
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const neabyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => Math.abs(value) <= 90),
+    longitude: z.number().refine((value) => Math.abs(value) <= 180),
+  });
+
+  const {
+    latitude,
+    longitude,
+  } = neabyGymsQuerySchema.parse(request.body);
+
+  const fetchNeabyGymsUseCase = makeFetchNeabyGymsUseCase();
+
+  const { gyms } = await fetchNeabyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+
+  return reply.status(200).send({
+    gyms,
+  });
+}
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,13 @@
 import { FastifyInstance } from 'fastify';
 import { verifyJWT } from '@/http/middlewares/verifyJWT';
+import { search } from './search';
+import { nearby } from './nearby';
+import { create } from './create';
 
 export async function gymRoutes(app: FastifyInstance) {
   app.addHook('preHandler', verifyJWT);
+
+  app.get('/search', search);
+  app.get('/nearby', nearby);
+  app.post('/', create);
 }
