35460a0e57ec99aa7340a5fd647d820196d1d16b
Matchobas <maalexandria@hotmail.com>
2023-08-03 21:53:26 -0300

    Chore: finished create check in controller

--- src/http/controllers/checkIns/create.ts
+++ src/http/controllers/checkIns/create.ts
@@ -1,8 +1,13 @@
 import { z } from 'zod';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { makeCheckInUseCase } from '@/useCases/factories/makeCheckInUseCase';
+import { MaxDistanceError } from '@/useCases/errors/maxDistanceError';
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createCheckInParamsSchema = z.object({
+    gymId: z.string().uuid(),
+  });
+
   const createCheckInBodySchema = z.object({
     latitude: z.number().refine((value) => Math.abs(value) <= 90),
     longitude: z.number().refine((value) => Math.abs(value) <= 180),
@@ -13,6 +18,8 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
       latitude,
       longitude,
     } = createCheckInBodySchema.parse(request.body);
+
+    const { gymId } = createCheckInParamsSchema.parse(request.params);
 
     const userId = request.user.sub;
 
@@ -27,7 +34,11 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
 
     return reply.status(201).send();
   } catch (err) {
-    console.log('todo');
+    if (err instanceof MaxDistanceError) {
+      return reply.status(400).send({
+        message: 'User in invalid distance',
+      });
+    }
     throw err;
   }
 }
