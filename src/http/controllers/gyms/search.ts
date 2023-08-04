b3f7701554e79994db932f5a43d5f0c1c834b1f3
Matchobas <maalexandria@hotmail.com>
2023-08-03 21:21:17 -0300

    Feat: gyms controllers

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { makeSearchGymsUseCase } from '@/useCases/factories/makeSearchGymsUseCase';
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const {
+    q,
+    page,
+  } = searchGymsQuerySchema.parse(request.body);
+
+  const searchGymsUseCase = makeSearchGymsUseCase();
+
+  const { gyms } = await searchGymsUseCase.execute({
+    query: q,
+    page,
+  });
+
+  return reply.status(200).send({
+    gyms,
+  });
+}
