f5c5a52c5e418b9f34ead6a94375495debe9d4dd
Matchobas <maalexandria@hotmail.com>
2023-08-07 16:58:09 -0300

    Chore: refresh token route init

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { verifyJWT } from '@/http/middlewares/verifyJWT';
 import { register } from './register';
 import { authenticate } from './authenticate';
 import { profile } from './profile';
+import { refresh } from './refresh';
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refresh);
 
   app.get('/me', {
     preHandler: [verifyJWT],
