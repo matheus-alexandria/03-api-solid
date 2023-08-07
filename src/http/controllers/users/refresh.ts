b5c52e274eefd905adb8d44c0fc26fa7c80e5e1c
Matchobas <maalexandria@hotmail.com>
2023-08-07 17:04:50 -0300

    Chore: refresh token route finished

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -1,53 +1,30 @@
-import { z } from 'zod';
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { InvalidCredentialsError } from '@/useCases/errors/invalidCredentialsError';
-import { makeAuthenticateUseCase } from '@/useCases/factories/makeAuthenticateUseCase';
 
 export async function refresh(request: FastifyRequest, reply: FastifyReply) {
-  const authenticateBodySchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
+  await request.jwtVerify({ onlyCookie: true });
+
+  const token = await reply.jwtSign({}, {
+    sign: {
+      sub: request.user.sub,
+    },
   });
 
-  const { email, password } = authenticateBodySchema.parse(request.body);
+  const refreshToken = await reply.jwtSign({}, {
+    sign: {
+      sub: request.user.sub,
+      expiresIn: '7d',
+    },
+  });
 
-  try {
-    const authenticateUseCase = makeAuthenticateUseCase();
-
-    const { user } = await authenticateUseCase.execute({
-      email,
-      password,
+  return reply
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true,
+      httpOnly: true,
+      sameSite: true,
+    })
+    .status(200)
+    .send({
+      token,
     });
-
-    const token = await reply.jwtSign({}, {
-      sign: {
-        sub: user.id,
-      },
-    });
-
-    const refreshToken = await reply.jwtSign({}, {
-      sign: {
-        sub: user.id,
-        expiresIn: '7d',
-      },
-    });
-
-    return reply
-      .setCookie('refreshToken', refreshToken, {
-        path: '/',
-        secure: true,
-        httpOnly: true,
-        sameSite: true,
-      })
-      .status(200)
-      .send({
-        token,
-      });
-  } catch (err) {
-    if (err instanceof InvalidCredentialsError) {
-      return reply.status(400).send({ message: err.message });
-    }
-
-    throw err;
-  }
 }
